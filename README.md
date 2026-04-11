520b58388bf667b4682171c5353ffc5be086587c
Hernande Monteiro <monteiro1998@live.com>
2026-04-11 16:19:53 -0300

    init: first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alura - Nodejs: dominando filas, pilhas e estruturas de dados
+
+Repositório com a intenção de colocar em prática conhecimentos do curso de NodeJS da Alura.
+
+Esse curso é um dos cursos que fazem parte da Carreira: Desenvolvimento Back-End Node.js, se apresentando no nível 3.
---
+++ queue.ts
@@ -0,0 +1,50 @@
+class Queue {
+  private items: any[] = [];
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  enqueue(item: any) {
+    this.items.push(item);
+  }
+
+  dequeue(): any {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty");
+    }
+    return this.items.shift();
+  }
+
+  peek(): any {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty");
+    }
+    return this.items[0];
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
+  show(): any[] {
+    return this.items;
+  }
+}
+
+const queue1 = new Queue();
+queue1.enqueue(1);
+queue1.enqueue(2);
+queue1.enqueue(3);
+queue1.enqueue(4);
+
+console.log("queue1.show():", queue1.show()); // Output: [1, 2, 3, 4]
+console.log("queue1.dequeue():", queue1.dequeue()); // Output: 1
+console.log("queue1.peek():", queue1.peek()); // Output: 2
+console.log("queue1.size():", queue1.size()); // Output: 3
+console.log("queue1.isEmpty():", queue1.isEmpty()); // Output: false
+queue1.dequeue();
+queue1.dequeue();
+queue1.dequeue();
+console.log("after dequeueing all elements!");
+console.log("queue1.isEmpty():", queue1.isEmpty()); // Output: true
